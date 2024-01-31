3baf81567533410046f2590c97f81e4bbbbbd1db
Бейсенбаева Диана <156782992+dibsnvas@users.noreply.github.com>
2024-01-31 10:32:27 +0600

    Update ex1.py

--- lab3/classes/ex1.py
+++ lab3/classes/ex1.py
@@ -1,9 +1,10 @@
 class string():
     def get_string(self):
-        self.s=input()
+        self.s = input()
 
     def print_string(self):
         print(self.s.upper())
+
 x = string()
 x.get_string()
-x.print_string
+x.print_string()
