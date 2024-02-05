9f5556ed2ca5ef08c9d1d078b4a8a472a3834745
Бейсенбаева Диана <156782992+dibsnvas@users.noreply.github.com>
2024-02-05 23:38:35 +0600

    Update ex1.py

--- lab3/func2/ex1.py
+++ lab3/func2/ex1.py
@@ -1,1 +1,7 @@
-
+def get_ratings(name):
+    for movie in movies:
+        if movie["name"] == name:
+            if movie["imdb"] > 5.5:
+                return True
+            else:
+                return False
