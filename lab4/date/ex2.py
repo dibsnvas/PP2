4e9992e066524af1bec50bc6729aa24a16873408
Бейсенбаева Диана <156782992+dibsnvas@users.noreply.github.com>
2024-02-08 17:03:05 +0600

    Create ex2.py

---
+++ lab4/date/ex2.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+current_date = datetime.now()
+new_date = current_date -timedelta(days=1)
+new_date2 = current_date + timedelta(days=1)
+print(new_date.strftime("%Y-%m-%d"))
+print(current_date.strftime("%Y-%m-%d"))
+print(new_date2.strftime("%Y-%m-%d"))
