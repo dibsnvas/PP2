12134688129420c498ed6624ecb441c49db99883
Бейсенбаева Диана <156782992+dibsnvas@users.noreply.github.com>
2024-02-12 20:11:16 +0600

    Create ex2.py

---
+++ lab4/math/ex2.py
@@ -0,0 +1,6 @@
+import math
+height = int(input("Height: "))
+base = int(input("Base, first value: "))
+base2 = int (input("Base, second value: "))
+area = (((base+base2)/2)*height)
+print("Expected Output: ", area)
