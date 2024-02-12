1e84092c94c35aa0b3d3f70020c2575f5363d828
Beisenbayeva Diana <156782992+dibsnvas@users.noreply.github.com>
2024-02-12 22:05:35 +0600

    Create ex1.py

---
+++ lab4/generators/ex1.py
@@ -0,0 +1,7 @@
+N=int(input())
+def generate_squares(N):
+    for i in range(1, N + 1):
+        yield i**2
+squares_generator = generate_squares(N)
+for square in squares_generator:
+    print(square)
