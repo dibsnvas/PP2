35fee3adecaeb9385db38dcf0261cf3003f12a4c
Beisenbayeva Diana <156782992+dibsnvas@users.noreply.github.com>
2024-02-26 23:30:46 +0600

    Create ex7.py

---
+++ lab5/ex7.py
@@ -0,0 +1,8 @@
+import re
+def snake_to_camel(snake_case):
+    return re.sub(r'_([a-z])', lambda x: x.group(1).upper(), snake_case)
+
+snake = "hello_everyone"
+camel_case = snake_to_camel(snake)
+
+print(f"{camel_case}")
