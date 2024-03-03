db3653067628d795b5ba5f3ed8247cfc886c9de7
Beisenbayeva Diana <156782992+dibsnvas@users.noreply.github.com>
2024-03-03 21:44:29 +0500

    Create ex3.py

---
+++ lab6/dir-and-files/ex3.py
@@ -0,0 +1,8 @@
+import os
+def test_path_details(path):
+    if os.path.exists(path):
+        print("Path exists.")
+        print("Directory:", os.path.dirname(path))
+        print("Filename:", os.path.basename(path))
+    else:
+        print("Path does not exist.")
